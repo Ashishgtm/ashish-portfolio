a231bcd53a52d3e41ba40c47f70440b324b96d6d
Ashish Gautam <ashishgautam9846@gmail.com>
2025-04-04 10:30:16 +0545

    changes in section A

--- src/components/Homecomps/SectionA.js
+++ src/components/Homecomps/SectionA.js
@@ -5,9 +5,12 @@ import './SectionA.css'
 const SectionA = () => {
   return (
 <div className="portfolio-header">
-      <div className="design-text">Design in Details</div>
+      <div className="design-text">Design. Develop. Deliver</div>
       <div className="portfolio-title">PORTFOLIO</div>
       <div className="name-text">Ashish Gtm</div>
+      <div className="scroll-down">
+        <div className="scroll-down-icon"></div>
+      </div>
     </div>
   )
 }
